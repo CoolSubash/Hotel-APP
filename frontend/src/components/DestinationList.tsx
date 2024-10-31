56ecfb7eada3c02858466464c86397921b686110
Subash Neupane <subashneupane@Subashs-MacBook-Pro.local>
2024-10-31 05:04:28 -0400

    Remove Error

--- frontend/src/components/DestinationList.tsx
+++ frontend/src/components/DestinationList.tsx
@@ -32,7 +32,7 @@ const NextArrow = (props: any) => {
   };
   
   const PrevArrow = (props: any) => {
-    const { =onClick } = props;
+    const { onClick } = props;
     return (
       <div className="absolute top-1/2 -left-3 transform -translate-y-1/2 z-10 cursor-pointer text-white">
         <button onClick={onClick} className="bg-blue-600 rounded-full shadow-md p-2">
